832cad0c1390319e9f802aba383938b084180557
odyssey02 <53847369+odyssey02@users.noreply.github.com>
2023-03-30 16:12:16 +0900

    Add files via upload

---
+++ 202344040_C/switch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    char op;
+    int x=0,y=0,result=0;
+    printf("수식을 입력하시오(예: 2 + 5) >> ");
+    scanf("%d %c %d",&x,&op,&y);
+
+    switch (op)
+    {
+    case '+':
+        result=x+y;
+        break;
+    case '-':
+        result=x-y;
+        break;
+    case '*':
+        result=x*y;
+        break;
+    case '/':
+        result=x/y;
+        break;
+    default:
+        printf("지원되지 않는 연산자입니다.\n");
+        break;
+    }
+
+    printf("%d %c %d = %d\n",x,op,y,result);
+
+    return 0;
+}
