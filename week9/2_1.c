939c33b7108c1d04d86e25c5e0bda5a184bdb9bf
odyssey02 <53847369+odyssey02@users.noreply.github.com>
2023-04-27 14:50:02 +0900

    Create 2_1.c

---
+++ week9/2_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int x=20,y=10;
+    int sum=x+y;
+    int dif=x-y;
+    int mul=x*y;
+    int div=x/y;
+    printf("두수의 합: %d\n",sum);
+    printf("두수의 차: %d\n",dif);
+    printf("두수의 곱: %d\n",mul);
+    printf("두수의 몫: %d\n",div);
+    
+    return 0;
+}
